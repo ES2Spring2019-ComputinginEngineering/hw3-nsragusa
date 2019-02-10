30001cbd80286b31b3132c6775250385bbac0852
nsragusa <nicolas.ragusa@tufts.edu>
2019-02-09 22:43:07 -0500

    Update bubble level.py

--- bubble level.py
+++ bubble level.py
@@ -1,3 +1,7 @@
+# YOUR NAME: Nicolas Ragusa
+# NUMBER OF HOURS TO COMPLETE: 4.5 hours
+# YOUR COLLABORATION STATEMENT(s): I worked with David Fricke and Patrick Wright.
+
 import microbit
 import math as m
 from microbit import *
@@ -8,37 +12,38 @@ while True:
     microbit.sleep(200)
     x1 = microbit.accelerometer.get_x()
     y1 = microbit.accelerometer.get_y()
-    z1 = 981 + microbit.accelerometer.get_z()
+    z1 = microbit.accelerometer.get_z()
     x2 = microbit.accelerometer.get_x()
     y2 = microbit.accelerometer.get_y()
-    z2 = 981 + microbit.accelerometer.get_z()
+    z2 = microbit.accelerometer.get_z()
     x3 = microbit.accelerometer.get_x()
     y3 = microbit.accelerometer.get_y()
-    z3 = 981 + microbit.accelerometer.get_z()
+    z3 = microbit.accelerometer.get_z()
     xa = (x1 + x2 + x3)/3
     ya = (y1 + y2 + y3)/3
-    radians1 = m.atan2(ya, xa)
+    za = (z1 + z2 + z3)/3
+    radians1 = m.atan2(ya, za)
     d1 = (radians1*180)/m.pi
-    radians2 = m.atan2(xa, ya)
+    radians2 = m.atan2(xa, za)
     d2 = (radians2*180)/m.pi
     print(d1, d2)
-    if (d1 < 1 and d1 > -1 and d2 < 1 and d2 > -1) or (d1 == -90 and d2== 180) or (d1 == 90 and d2 == 0) or (d1 == 45 and d2 == 45) or (d1 == 0 and d2 == 90) or ( d1 == 180 and d2 == 180) :
+    if (abs(d1) + abs(d2)) > 350:
         microbit.display.show(plus)
-    elif d1 < -70 and (d2 > 165 or d2 < -160) :
+    elif d1 < -120 and (d2 > 170 or d2 < -175) :
         microbit.display.show(Image.ARROW_N)
-    elif d1 < 10 and d1 > -10 and d2 > 60:
-        microbit.display.show(Image.ARROW_E)
-    elif (d1 > 75 and d1 < 100 and d2 < 20 and d2 > -12) :
-        microbit.display.show(Image.ARROW_S)
-    elif d1 < -150 and d2 < -70 and d2 > -115 :
-        microbit.display.show(Image.ARROW_W)
-    elif d1 < -105 and d1 > -155 and d2 < -110 and d2 > -160 :
+    elif d1 < -130 and d2 < -100 :
         microbit.display.show(Image.ARROW_NW)
-    elif d1 > -90 and d1 <-10 :
-        microbit.display.show(Image.ARROW_NE)
-    elif d1 > 90 :
+    elif (d1 > 170 or d1 < -170) and d2 < -110:
+        microbit.display.show(Image.ARROW_W)
+    elif d1 > 135 and d2 < -100:
         microbit.display.show(Image.ARROW_SW)
-    elif d1 < 70 and d1 > 20 :
+    elif d1 > 100 and d2 > 160:
+        microbit.display.show(Image.ARROW_S)
+    elif d1 > 150 and d2 > 120:
         microbit.display.show(Image.ARROW_SE)
+    elif d1 < -160 and d2 > 120:
+        microbit.display.show(Image.ARROW_E)
+    elif d1 < -120 and d2 > 100:
+        microbit.display.show(Image.ARROW_NE)
     else:
-        microbit.display.show(plus)
+        microbit.display.show(Image.CONFUSED)
